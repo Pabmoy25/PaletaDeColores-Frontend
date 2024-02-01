7abfa4db87aa4e66d8a07624847a37fcc2ea81e0
Pabmoy25 <pabmoy1725@gmail.com>
2024-02-01 09:56:44 -0300

    Se crea el componente Footer y se realiza su maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css'
+import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
-  
-
   return (
     <>
-     
+    <section className="mainpage"></section>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
